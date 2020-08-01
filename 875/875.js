1709b3b9cc0f5f85f3cb8972638e23ae51e6d80c
Hu Qiyang <hqy1012927920@163.com>
2020-08-01 16:54:57 +0800

    LC875

--- 875/875.js
+++ 875/875.js
@@ -6,12 +6,51 @@
  */
 
 
-var minEatingSpeed = function (piles, H) {
-    let low = 1,
-        high=Math.max(...piles);
-    console.log(low,high)
-    //求出最大香蕉数，在某把之中
-    return low
+var cal = function (piles, H, high, low, mid) {
+    let time = 0   
+    if((high-low)==1){
+        return high
+    }
+    mid = Math.ceil((low + high) / 2)
+    for (let i = 0; i < piles.length; i++) {
+        time += Math.ceil(piles[i] / mid)
+    }
+    
+    if (time < H) {
+        high = mid
+    }
+    else if (time > H) {
+        low = mid
+    }
+    else if(time==H){
+        return mid
+    }
+    
+    return cal(piles, H, high, low, mid)
+}
+
+var c = function(piles,H,spd){
+    let time1 = 0,
+        time2 = 0;
+    for (let i = 0; i < piles.length; i++) {
+        time1 += Math.ceil(piles[i] / (spd))
+    }
+    for (let j = 0; j < piles.length; j++) {
+        time2 += Math.ceil(piles[j] / (spd-1))
+    } 
+    if((time1<=H)&&!(time2==H)) return spd
+    else {
+    spd-=1;
+    return c(piles,H,spd)
+    }
+}
+
+var minEatingSpeed = function (piles, H) { 
+    let high = Math.max(...piles),
+        low = 0,
+        spd = Math.ceil(cal(piles, H, high, low));
+    if((spd==2)||(spd==1)) return spd
+    else return c(piles,H,spd)   
 };
 
 minEatingSpeed([3, 6, 7, 11], 8);
